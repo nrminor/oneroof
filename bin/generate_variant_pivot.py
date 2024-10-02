1be8547b774f08f14a5b461bd3fa970febf8f562
Nicholas Minor <nrminor@wisc.edu>
2024-10-02 11:38:02 -0500

    adding some template polars tsv parsing to variant pivot script

--- bin/generate_variant_pivot.py
+++ bin/generate_variant_pivot.py
@@ -4,6 +4,7 @@
 import argparse
 from pathlib import Path
 
+import polars as pl
 from loguru import logger
 
 
@@ -27,7 +28,30 @@ def main() -> None:
     """
     TODO
     """
-    _ = parse_command_line_args()
+    args = parse_command_line_args()
+    input_table = args.input_table
+
+    _ = pl.read_csv(
+        input_table,
+        separator="\t",
+        has_header=False,
+        skip_rows=1,
+        columns=[
+            "contig",
+            "ref",
+            "pos",
+            "alt",
+            "af",
+            "ac",
+            "dp",
+            "mq",
+            "gene",
+            "aa_effect",
+            "ref_codon_alt",
+            "cds_pos",
+            "aa_pos",
+        ],
+    ).with_columns(pl.col("aa_effect").str.replace(".p", "").alias("aa_effect"))
     logger.info("Hi mom!")
 
 
