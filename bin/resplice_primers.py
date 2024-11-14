660686a38a8264f3d4da34bbfb820f472086a3b7
Nicholas Minor <nicholasrminor@gmail.com>
2024-11-14 12:04:09 -0600

    primer resplicing is becoming quite robust

--- bin/resplice_primers.py
+++ bin/resplice_primers.py
@@ -5,13 +5,15 @@ Module that finds all possible combinations of spike-in primers in an amplicon s
 
 Example usage:
 ```
-usage: resplice_primers.py [-h] --input_bed INPUT_BED [--output_prefix OUTPUT_PREFIX] [--fwd_suffix FWD_SUFFIX]
-                           [--rev_suffix REV_SUFFIX] [--idx_delim IDX_DELIM] [--idx_position IDX_POSITION]
+usage: resplice_primers.py [-h] --input_bed INPUT_BED [--output_prefix OUTPUT_PREFIX]
+                           [--fwd_suffix FWD_SUFFIX] [--rev_suffix REV_SUFFIX]
+                           [--idx_delim IDX_DELIM] [--idx_position IDX_POSITION] [-v]
 
 options:
   -h, --help            show this help message and exit
   --input_bed INPUT_BED, -i INPUT_BED
-                        BED file with one-off spike-in primers to be respliced into possible amplicons.
+                        BED file with one-off spike-in primers to be respliced into
+                        possible amplicons.
   --output_prefix OUTPUT_PREFIX, -o OUTPUT_PREFIX
                         Output prefix for final respliced amplicon BED file.
   --fwd_suffix FWD_SUFFIX, -f FWD_SUFFIX
@@ -19,19 +21,25 @@ options:
   --rev_suffix REV_SUFFIX, -r REV_SUFFIX
                         The suffix to be expected in the names for reverse primers
   --idx_delim IDX_DELIM, -d IDX_DELIM
-                        The symbol used to delimit the index of a spike-in primer, which differentiates it from the
-                        primer original around the same position. Defaults to a hyphen/dash: '-'.
+                        The symbol used to delimit the index of a spike-in primer,
+                        which differentiates it from the primer original around the
+                        same position. Defaults to a hyphen/dash: '-'.
   --idx_position IDX_POSITION, -p IDX_POSITION
-                        The position where the primer spike-in index should be expected, defaulting to the final
-                        position after splitting by the specified index delimiter symbol.
+                        The position where the primer spike-in index should be
+                        expected, defaulting to the final position after splitting by
+                        the specified index delimiter symbol.
+  -v, --verbose         Increase verbosity level (-v for WARNING, -vv for INFO, -vvv
+                        for DEBUG)
 ```
 """
 
 from __future__ import annotations
 
 import argparse
+import sys
 from itertools import product
 from pathlib import Path
+from pprint import pformat
 from typing import Any
 
 import polars as pl
@@ -52,6 +60,7 @@ def parse_command_line_args() -> argparse.Namespace:
         --rev_suffix/-r: The suffix expected in reverse primer names.
         --idx_delim/-d: The symbol delimiting spike-in primer indices.
         --idx_position/-p: Position to expect primer spike-in index after splitting by delimiter.
+        --verbose/-v: Increase verbosity level (-v for WARNING, -vv for INFO, -vvv for DEBUG)
     """
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -59,8 +68,7 @@ def parse_command_line_args() -> argparse.Namespace:
         "-i",
         type=Path,
         required=True,
-        help="BED file with one-off spike-in primers to be respliced into possible\
-        amplicons.",
+        help="BED file with one-off spike-in primers to be respliced into possible amplicons.",
     )
     parser.add_argument(
         "--output_prefix",
@@ -92,9 +100,7 @@ def parse_command_line_args() -> argparse.Namespace:
         type=str,
         required=False,
         default="-",
-        help="The symbol used to delimit the index of a spike-in primer, which \
-        differentiates it from the primer original around the same position. Defaults \
-        to a hyphen/dash: '-'.",
+        help="The symbol used to delimit the index of a spike-in primer, which differentiates it from the primer original around the same position. Defaults to a hyphen/dash: '-'.",
     )
     parser.add_argument(
         "--idx_position",
@@ -102,12 +108,59 @@ def parse_command_line_args() -> argparse.Namespace:
         type=int,
         required=False,
         default=-1,
-        help="The position where the primer spike-in index should be expected, \
-        defaulting to the final position after splitting by the specified index \
-        delimiter symbol.",
+        help="The position where the primer spike-in index should be expected, defaulting to the final position after splitting by the specified index delimiter symbol.",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        help="Increase verbosity level (-v for WARNING, -vv for INFO, -vvv for DEBUG)",
     )
 
     return parser.parse_args()
+
+
+def check_bed_existence(provided_bed_path: str | Path) -> None:
+    logger.info(f"Parsing input BED file at {provided_bed_path}...")
+    if not Path(provided_bed_path).is_file():
+        available_bed_files = [
+            str(file) for file in Path(provided_bed_path).parent.glob("*.bed")
+        ]
+        if len(available_bed_files) == 0:
+            logger.error(
+                f"The provided input BED file, {provided_bed_path}, does not exist or is not a file. Aborting.",
+            )
+            sys.exit(1)
+        logger.error(
+            f"The provided input BED file, {provided_bed_path}, does not exist or is not a file. Perhaps you meant one of the following files?\n\n{pformat(available_bed_files, indent=1)}",
+        )
+        sys.exit(1)
+
+
+def check_idx_delims(
+    parsed_bed: pl.DataFrame,
+    idx_delim: str = "-",
+    _idx_position: int = -1,
+) -> None:
+    primer_names = parsed_bed.select("NAME").to_series().to_list()
+
+    warning_list = []
+
+    for name in primer_names:
+        delim_count = name.count(idx_delim)
+
+        if delim_count <= 1:
+            continue
+
+        warning_list.append(name)
+
+    if len(warning_list) == 0:
+        return
+
+    logger.warning(
+        f"{len(warning_list)} primer names contained more than one of the symbol used to delimit spike-in primer index, '{idx_delim}', which is a special value in this program. Unexpected behavior, such as merging multiple amplicons into one or incorrect resplicing, is likely to occur.\n\nTo fix it, either use a different delimiter symbol than '{idx_delim}' or modify the primer names so that the symbol '{idx_delim}' is only used to denote a spike-in index.\n\nBelow is the list of {len(warning_list)} primers that generated this warning:\n\n{pformat(warning_list, indent=4)}",
+    )
 
 
 def partition_by_amplicon(
@@ -285,7 +338,7 @@ def normalize_indices(
     for primer_df in partitioned_bed:
         # partition the dataframe for the current amplicon by name, which should
         # effectively mean partitioning by forward or reverse.
-        new_dfs = primer_df.partition_by("NAME")
+        new_dfs: list[pl.DataFrame] = primer_df.partition_by("NAME")
 
         # the inner loop iterates through each group of potentially redundant primer
         # names, assigning new indices to differentiate them if they are. For example,
@@ -293,6 +346,15 @@ def normalize_indices(
         # primers), they should all have the same name at this stage, and will thus have
         # -1, -2, and -3 appended to them respectively.
         for j, primer_name_df in enumerate(new_dfs):
+            # run a check to make sure the expectation that entries in this NAME-
+            # partitioned dataframe all have the same name at this stage.
+            if len(primer_name_df.select("NAME").unique().to_series().to_list()) != 1:
+                logger.warning(
+                    f"Unable to properly assign indices for these primers: {primer_name_df.select('NAME').unique().to_series().to_list()}. Skipping. Please open an issue if the program should crash here rather than skipping.",
+                )
+                new_dfs[j] = primer_name_df.clear()
+                continue
+
             # run a check for whether there's >= 1 spikein. If there are none, there
             # will just be one primer per name partition, in which case we can skip to
             # the next primer set without renaming anything. We retain this duplicate
@@ -307,22 +369,12 @@ def normalize_indices(
                 .to_series()
                 .to_list()
             )
-            if True not in dupe_check and dupe_check.count(False) <= 1:
-                # NAME modifications are no longer needed, so we may now revert the
-                # primer name to its original state
-                new_dfs[j] = primer_name_df.with_columns(
-                    pl.col("ORIG_NAME").alias("NAME"),
-                )
-                continue
-
-            # run a check to make sure the expectation that entries in this NAME-
-            # partitioned dataframe all have the same name at this stage.
-            if len(primer_name_df.select("NAME").unique().to_series().to_list()) != 1:
-                logger.warning(
-                    f"Unable to properly assign indices for these primers: \
-                    {primer_name_df.select('NAME').unique().to_series().to_list()}. \
-                    Skipping. Please open an issue if the program should crash here \
-                    rather than skipping.",
+            assert dupe_check.count(False) <= 1
+            if True not in dupe_check and primer_name_df.shape[0] == 1:
+                new_dfs[j] = assign_new_indices(
+                    primer_name_df,
+                    fwd_suffix,
+                    rev_suffix,
                 )
                 continue
 
@@ -411,16 +463,16 @@ def resolve_primer_names(
 
         # run checks to make sure indices that could be used for tracking each pair are
         # parseable from the primer name
-        assert _convertable_to_int(
-            fwd_final_element,
-        ), f"The primer {old_fwd_primer} does not end with a hyphen-delimited integer, \
-        e.g. '-1', which is required for properly handling different possible primer \
-        combinations. Aborting."
-        assert _convertable_to_int(
-            rev_final_element,
-        ), f"The primer {old_rev_primer} does not end with a hyphen-delimited integer, \
-        e.g. '-1', which is required for properly handling different possible primer \
-        combinations. Aborting."
+        if not _convertable_to_int(fwd_final_element):
+            logger.error(
+                f"The primer {old_fwd_primer}, which has been paired with {old_rev_primer} does not end with a hyphen-delimited integer, e.g. '-1', which is required for properly handling different possible primer combinations. Printing the set of combinations being handled and aborting:\n\n{all_possible_pairs}",
+            )
+            sys.exit(1)
+        if not _convertable_to_int(rev_final_element):
+            logger.error(
+                f"The primer {old_rev_primer}, which has been paired with {old_fwd_primer} does not end with a hyphen-delimited integer, e.g. '-1', which is required for properly handling different possible primer combinations. Printing the set of combinations being handled and aborting:\n\n{all_possible_pairs}",
+            )
+            sys.exit(1)
 
         # figure out which combination of primers is being handled and check
         # whether it has already been handled
@@ -503,19 +555,14 @@ def resplice_primers(
             # within the pair
             pair_labels = primer_df.select("NAME").to_series().to_list()
             logger.debug(
-                f"Pair of primers within the amplicon {amplicon} detected: \
-                {pair_labels}. No resplicing will be needed here, though double check \
-                that the necessary forward and reverse suffixes, {fwd_suffix} and \
-                {rev_suffix}, are present.",
+                f"Pair of primers within the amplicon {amplicon} detected: {pair_labels}. No resplicing will be needed here, though double check that the necessary forward and reverse suffixes, {fwd_suffix} and {rev_suffix}, are present.",
             )
             assert any(
                 fwd_suffix in primer for primer in pair_labels
-            ), f"The forward suffix {fwd_suffix} is missing in the provided primer \
-            pairs: {pair_labels}. Aborting."
+            ), f"The forward suffix {fwd_suffix} is missing in the provided primer pairs: {pair_labels}. Aborting."
             assert any(
                 rev_suffix in primer for primer in pair_labels
-            ), f"The reverse suffix {rev_suffix} is missing in the provided primer \
-            pairs: {pair_labels}. Aborting."
+            ), f"The reverse suffix {rev_suffix} is missing in the provided primer pairs: {pair_labels}. Aborting."
 
             # if so, all is well. Append the primers into the growing list of correct
             # dataframes and move onto the next amplicon
@@ -538,6 +585,9 @@ def resplice_primers(
 
         # compute the new names for the primers that will be used to expand shared
         # primers across all spike-ins
+        logger.debug(
+            f"Resplicing will be performed for the following primers:\n\n{pformat(fwd_primers + rev_primers, indent=1)}",
+        )
         old_primer_names, new_primer_names = resolve_primer_names(
             fwd_primers,
             rev_primers,
@@ -641,6 +691,24 @@ def main() -> None:
     """
     args = parse_command_line_args()
 
+    # set up logger based on the requested verbosity
+    match args.verbose:
+        case 0:
+            level = "WARNING"
+        case 1:
+            level = "SUCCESS"
+        case 2:
+            level = "INFO"
+        case 3:
+            level = "DEBUG"
+        case _:
+            level = "WARNING"
+    logger.remove()
+    logger.add(sys.stderr, colorize=True, level=level)
+    logger.info(f"Logging set to {level}")
+
+    # read in the BED file
+    check_bed_existence(args.input_bed)
     bed_df = pl.read_csv(
         args.input_bed,
         separator="\t",
@@ -655,6 +723,14 @@ def main() -> None:
         ],
     )
 
+    # look through primer names before proceeding to make sure primers aren't named
+    # inconsistently with respect the symbols they contain
+    check_idx_delims(
+        bed_df,
+        args.idx_delim,
+        args.idx_position,
+    )
+
     # identify amplicons/primer pairings and create a separate dataframe for the primers
     # associated with each
     partitioned_bed = partition_by_amplicon(
@@ -662,15 +738,24 @@ def main() -> None:
         args.fwd_suffix,
         args.rev_suffix,
     )
+    logger.info(
+        f"{len(bed_df)} input primers split into {len(partitioned_bed)} discrete amplicons.",
+    )
 
     # make sure that all primers have informative names in amplicons where there are
     # spiked-in primers
+    logger.info(
+        f"Re-assigning 1-based indices to account for spike-ins amongst the {len(bed_df)} input primers.",
+    )
     indexed_primer_dfs = normalize_indices(
         partitioned_bed,
         args.fwd_suffix,
         args.rev_suffix,
     )
 
+    logger.info(
+        "Re-indexing successful. Proceeding to resplicing any implicit combinations that are not accounted for with a row in the input BED file.",
+    )
     respliced_dfs = resplice_primers(
         indexed_primer_dfs,
         args.fwd_suffix,
@@ -679,16 +764,16 @@ def main() -> None:
         args.idx_position,
     )
 
-    if len(respliced_dfs) == len(indexed_primer_dfs):
+    if len(respliced_dfs) != len(indexed_primer_dfs):
         logger.warning(
-            f"The number of amplicon primer sets that made it through resplicing,\
-            {len(respliced_dfs)}, does not match the number of input amplicons, \
-            {len(indexed_primer_dfs)}. Data loss may have occurred for this primer \
-            set.",
+            f"The number of amplicon primer sets that made it through resplicing, {len(respliced_dfs)}, does not match the number of input amplicons, {len(indexed_primer_dfs)}. Data loss may have occurred for this primer set.",
         )
 
     # run one last check that each group has at least one identifiable forward primer
     # and one identifiable reverse primer, if not multiple.
+    logger.info(
+        f"Finalizing primer pairings amongst {len(respliced_dfs)} primers by verifying that every forward primer comes with a reverse primer.",
+    )
     final_df = finalize_primer_pairings(
         respliced_dfs,
         args.fwd_suffix,
@@ -697,7 +782,11 @@ def main() -> None:
 
     # drop the now unnecessary columns, sort by start and stop position, and write
     # out with the user-provided output prefix
+    logger.info(
+        f"The input BED file contained {bed_df.shape[0]} primers. After searching for any spike-ins to resplice, the final BED file contains {final_df.shape[0]}",
+    )
     final_df.drop("Amplicon").drop("ORIG_NAME").sort(
+        "Ref",
         "Start Position",
         "Stop Position",
     ).write_csv(
